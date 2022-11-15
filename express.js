245258d5bbdfe885f7fa22a74cc609ead7cf6882
Sergey <sergey@example.com>
2022-11-15 11:30:28 +0700

    Авторизация

--- express.js
+++ express.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+var sessionHandler;
 
 const app = express()
+
+app.use(bodyParser.json())
+
+const PORT = 3000
 
 var users = [
   {
@@ -20,6 +30,22 @@ var users = [
   },
 
 ]
+
+//var sessionHandler = require('./public/js/authorization.js')
+
+const urlencodedParser = express.urlencoded({extended: false});
+
+app.use(cookieParser());
+
+app.use(session({
+ // store: store,
+  username: undefined,
+  password: undefined,
+  resave:  false,
+  saveUninitialized: true,
+  secret: "supersecret",
+    
+}))
 
 var sqlite3 = require('sqlite3').verbose()
 let sql;
@@ -53,12 +79,43 @@ app.get('/sandbox', (req, res) => {
     res.render('code-editor')
 })
 
-app.get('/user/:username', (req, res) => {
+app.get('/user', (req, res) => {
     console.log(req.headers['cookie'])
-    res.render('user', {username: req.params.username})
+    res.render('user', {username: req.session.username})
 })
 
-const PORT = 3000
+
+app.post('/register', urlencodedParser, function(req, res) {
+     var foundUser;
+     // поиск пользователся в базе данных;
+     if(req.session.username !== undefined && !req.session.password !== undefined)
+     {
+        
+     }
+     for(var i = 0; i<users.length; i++)
+     {
+      var u = users[i]
+      if(u.username == req.body.username && u.password == req.body.password)
+        {
+          foundUser = u.username
+          req.session.username = u.username
+          req.session.password = u.password
+          break
+        }
+     }
+     
+     // если ли логин пользователя в системе
+     if(foundUser !== undefined)
+     {
+       // req.session.username = foundUser
+        res.send(`Зашел пользователь ${req.body.username} с паролем ${req.body.password}`)
+     }
+     else
+     {
+        console.log("Login failed ", req.body.username)
+        res.status(401).send('Login error')
+     }
+})
 
 app.listen(PORT, () => {
 
--- public/js/code-script.js
+++ public/js/code-script.js
@@ -9,12 +9,46 @@ function eraseTheText(string) {
 
  function runCode() {
   alert("Выполнение кода");
+
  } 
 
- /* document.getElementById('enter').onclick =function() {
-   var usernameCookie = document.getElementById('userLogin').value;
-   var passwordCookie = document.getElementById('userPassword').value;
 
-   var cookie =  `${usernameCookie}=${passwordCookie}`;
-   document.cookie = cookie; 
-}; */
+ window.onload = function() {
+  const buttonToBeClicked = document.getElementById("enter");
+  var inputs = document.getElementsByTagName("input");
+
+  //var usernameCookie = document.getElementById('userLogin').value;
+  //var passwordCookie = document.getElementById('userPassword').value;
+
+  buttonToBeClicked.addEventListener('click', function() {
+   // alert("Выполнение кода");
+  //   var xhr=new XMLHttpRequest();
+
+  //   xhr.open('POST', '/sandbox');
+
+  //   var userData = {
+  //     username: inputs[0].value,
+  //     password: inputs[1].value
+  //   }
+  //   alert(inputs[0].value + ' ' + inputs[1].value);
+  //   xhr.setRequestHeader('Content-Type', 'application/json');
+  //   xhr.send(JSON.stringify(userData));
+    
+    
+  //   xhr.onload = function() {
+  //     var resultText = XMLHttpRequest.responseText;
+  //     alert(resultText)
+  //   }
+
+  //   xhr.onerror = function() {
+  //     alert('server error!')
+  //   }
+  //  //var cookie =  `${usernameCookie}=${passwordCookie}`;
+  //  // document.cookie = cookie; 
+
+  })
+
+
+ }
+
+
